bd099f2334b0a501ab47aa761ba8d1a955f79f4e
Saba. SM <49869460+BayoDino@users.noreply.github.com>
2020-12-23 17:21:45 +0330

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  command: 'c!',
+  game: {
+            name: "c!help",
+            type: "PLAYING"
+        },
+  token: process.env.DISCORD_TOKEN,
+  owner:'ownerID', // owner id goes here
+  status: 'online'
+}
