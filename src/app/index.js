b001b22a203a3c14662c49bba0abc4998d37c760
Saba <cranterns@gmail.com>
2021-03-17 15:25:14 +0330

    Create index.js

---
+++ src/app/index.js
@@ -0,0 +1,90 @@
+const { ipcMain, app, BrowserWindow, Menu } = require('electron');
+const client = require(__dirname+'/src/app/drp.js')('635084414543462412');
+
+function createWindow () {
+  const win = new BrowserWindow({
+	backgroundColor: '#7289da',
+	show: false,
+    width: 500,
+    height: 300,
+    webPreferences: {
+      nodeIntegration: true
+    },
+    titleBarStyle: 'customButtonsOnHover', frame: false,
+  })
+
+  win.loadFile('loading.html');
+  win.once('ready-to-show', () => {
+  win.show();
+  //then create the main page
+  const mainwin = new BrowserWindow({
+	backgroundColor: '#36393f',
+	show: false,
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    },
+	titleBarStyle: 'hiddenInset',
+	//frame: false,
+  });
+  mainwin.webContents.on('new-window', function(e, url) {
+	e.preventDefault();
+	require('electron').shell.openExternal(url);
+  });
+  mainwin.loadFile('index.html');
+  mainwin.once('ready-to-show', () => {
+    win.close();
+    mainwin.show();
+	try{
+		client.updatePresence({
+			state: 'Idling',
+			details: 'A Bot maker for Discord',
+			largeImageKey: 'logo-big',
+			instance: true,
+		});
+	}catch{
+	console.log("Failed");
+	}
+  });
+});
+}
+
+app.whenReady().then(()=>{
+	createWindow();
+	setMainMenu();
+	})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+ipcMain.on('close-me', (evt, arg) => {
+  app.quit();
+})
+
+function setMainMenu() {
+  const template = [
+    {
+      label: 'App',
+      submenu: [
+        {
+          label: 'About',
+          accelerator: 'Shift+CmdOrCtrl+H',
+          click() {
+              console.log('Oh, hi there!')
+          }
+        }
+      ]
+    }
+  ];
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+}
